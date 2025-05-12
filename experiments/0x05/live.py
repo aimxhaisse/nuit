dc9ee2b1cdf02321683723f928d4e2b14b2f44cb
s. rannou <mxs@sbrk.org>
2025-05-12 09:31:03 +0200

    feat: add HH explorations

---
+++ experiments/0x05/live.py
@@ -0,0 +1,58 @@
+# █▀ █▀█ █ █▀█
+# ▄█ █▄█ █ █▀▄ @ https://soir.dev
+
+@live()
+def setup():
+    bpm.set(115)
+
+    tracks.setup({
+        'hh': tracks.mk_sampler(fxs={
+            'hpf': fx.mk_hpf(cutoff=0.6, resonance=0.0),
+        })
+    })
+
+sp = sampler.new('drumnibus')
+mode = 0
+flavor = 0
+
+def hh_exploration_0():
+    patterns = [
+        '1...1...2....3..',
+    ]
+
+    samples = {
+        '1': {
+            'sp': 'oh-draconisd921',
+            'args': {
+                'end': 0.13,
+            }
+        },
+        '2': {
+            'sp': 'oh-draconisd922',
+            'args': {
+                'end': 0.07,
+                'rate': 0.96,
+            }
+        },
+        '3': {
+            'sp': 'oh-draconisd922',
+            'args': {
+                'end': 0.25,
+                'rate': 0.96,
+            }
+        }
+    }
+    
+    for i in range(16):
+        c = patterns[flavor][i]
+        if c in samples:
+            sp.play(samples[c]['sp'], **samples[c]['args'])
+        sp.play('oh')
+        sleep(1/4)
+
+
+@loop('hh', beats=4)
+def hh_explorations():
+    match mode:
+        case 0:
+            hh_exploration_0()
