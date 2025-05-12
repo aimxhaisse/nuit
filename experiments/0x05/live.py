e98d5f8c65b9e50cea41a15c18e8859dbdf82a23
s. rannou <mxs@sbrk.org>
2025-05-12 09:36:32 +0200

    fix: nit

--- experiments/0x05/live.py
+++ experiments/0x05/live.py
@@ -7,7 +7,7 @@ def setup():
 
     tracks.setup({
         'hh': tracks.mk_sampler(fxs={
-            'hpf': fx.mk_hpf(cutoff=0.6, resonance=0.0),
+            'hpf': fx.mk_hpf(cutoff=0.7, resonance=0.0),
         })
     })
 
@@ -17,29 +17,21 @@ flavor = 0
 
 def hh_exploration_0():
     patterns = [
-        '1...1...2....3..',
+        '1...2....3..1...',
     ]
 
     samples = {
         '1': {
             'sp': 'oh-draconisd921',
-            'args': {
-                'end': 0.13,
-            }
+            'args': {'end': 0.13}
         },
         '2': {
             'sp': 'oh-draconisd922',
-            'args': {
-                'end': 0.07,
-                'rate': 0.96,
-            }
+            'args': {'end': 0.07, 'rate': 0.96}
         },
         '3': {
             'sp': 'oh-draconisd922',
-            'args': {
-                'end': 0.25,
-                'rate': 0.96,
-            }
+            'args': {'end': 0.25, 'rate': 0.96}
         }
     }
     
